5265ff4a2140f72cd0e0d25ad72a1d6977b1838a
Nguyen Tuan Dung <dungnt8805@gmail.com>
2022-06-16 08:55:13 +0700

    XTRIP-87

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:xtrip_mobile/navigators/app_navigator.dart';
 import 'package:xtrip_mobile/repositories/auth_repository.dart';
-// import 'package:xtrip_mobile/screens/forgot_password_screen.dart';
-// import 'package:xtrip_mobile/screens/siginin_screen.dart';
-// import 'package:xtrip_mobile/screens/signup_screen.dart';
-// import 'package:xtrip_mobile/screens/splash_screen.dart';
 import 'package:xtrip_mobile/sessions/session_cubit.dart';
-
 
 Future main() async {
   await dotenv.load(fileName: '.env');
--- lib/sessions/session_cubit.dart
+++ lib/sessions/session_cubit.dart
@@ -1,13 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:xtrip_mobile/repositories/auth_repository.dart';
 import 'package:xtrip_mobile/sessions/session_state.dart';
 
 class SessionCubit extends Cubit<SessionState> {
   final AuthRepository authRepo;
   SessionCubit({required this.authRepo}) : super(UnknownSessionState()) {
-    attemptAutoLogin();
+    checkPermissions();
   }
-  void attemptAutoLogin () async {
+
+  Future<void> checkPermissions() async {
+    if (!(await Permission.locationAlways.request().isGranted)) {
+      emit(UnAuthorizedPermissions());
+    } else {
+      try {
+        await authRepo.attemptAutoLogin();
+        emit(Authenticated(user: null));
+      } on Exception {
+        emit(Unauthenticated());
+      }
+    }
+  }
+
+  void attemptAutoLogin() async {
     try {
       await authRepo.attemptAutoLogin();
       emit(Authenticated(user: null));
